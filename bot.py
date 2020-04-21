6d3f6ea7018449984050cc00cec70b124c85386c
Alex <coder.django@gmail.com>
2020-04-21 17:11:50 +0600

    added coronavirus cases worldwide

--- bot.py
+++ bot.py
@@ -13,22 +13,23 @@ def flag(code):
     return chr(ord(code[0]) + OFFSET) + chr(ord(code[1]) + OFFSET)
 
 
+@bot.message_handler(commands=['help'])
+def get_help(message):
+    bot.send_message(parse_mode='HTML',
+                     chat_id=message.chat.id,
+                     text="<b>Все страны: /covid</b> \n<b>Конкретная страны: /covid country_name </b>")
+
+
 @bot.message_handler(commands=['covid'])
 def get_coronavirus_information(message):
     text = message.text[7:]
-    with open('countries.json', 'r') as f:
-        countries_dict = json.load(f)
-    try:
-        country = [obj for obj in countries_dict if text == obj['en_name'] or text == obj['ru_name']]
+    if len(text) == 0:
         r = requests.get(
-            'https://corona.lmao.ninja/v2/countries/' + country[0]['en_name'])
+            'https://corona.lmao.ninja/v2/all')
         response = r.json()
         bot.send_message(parse_mode='HTML',
                          chat_id=message.chat.id,
-                         text="<b> Страна: "
-                              + flag(response['countryInfo']['iso2'])
-                              + " " + text
-                              + ".</b> \n Всего случайев: "
+                         text="Всего случайев: "
                               + str(response['cases'])
                               + "\n За сегодня случайев: "
                               + str(response['todayCases'])
@@ -38,11 +39,43 @@ def get_coronavirus_information(message):
                               + str(response['todayDeaths'])
                               + "\n Выздоровевших: "
                               + str(response['recovered'])
+                              + "\n Активных: "
+                              + str(response['active'])
+                              + "\n Критичных: "
+                              + str(response['critical'])
                               + "\n Протестированных: "
                               + str(response['tests'])
+                              + "\n Пострадавших стран: "
+                              + str(response['affectedCountries'])
                          )
-    except:
-        bot.send_message(message.chat.id, text="Такой страны нет в базе.")
+    else:
+        with open('countries.json', 'r') as f:
+            countries_dict = json.load(f)
+        try:
+            country = [obj for obj in countries_dict if text == obj['en_name'] or text == obj['ru_name']]
+            r = requests.get(
+                'https://corona.lmao.ninja/v2/countries/' + country[0]['en_name'])
+            response = r.json()
+            bot.send_message(parse_mode='HTML',
+                             chat_id=message.chat.id,
+                             text="<b> Страна: "
+                                  + flag(response['countryInfo']['iso2'])
+                                  + " " + text
+                                  + ".</b> \n Всего случайев: "
+                                  + str(response['cases'])
+                                  + "\n За сегодня случайев: "
+                                  + str(response['todayCases'])
+                                  + "\n Всего смертей: "
+                                  + str(response['deaths'])
+                                  + "\n За сегодня смертей: "
+                                  + str(response['todayDeaths'])
+                                  + "\n Выздоровевших: "
+                                  + str(response['recovered'])
+                                  + "\n Протестированных: "
+                                  + str(response['tests'])
+                             )
+        except:
+            bot.send_message(message.chat.id, text="Такой страны нет в базе.")
 
 
 if __name__ == '__main__':
